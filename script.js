b4e4aa53d6e6746d3162e5c67dd75553d218b888
Tran Dang Duat <23020023@vnu.edu.vn>
2023-11-06 23:38:38 +0700

    Add functions for buttons

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 let numbers = document.getElementById("numbers");
 let operators = document.getElementById("operators");
+let preAnswer = document.getElementById("pre-ans");
+let current = document.getElementById("current");
+let equalButton = document.getElementById("equal-btn");
+let allClearButton = document.getElementById("allclear");
+let clearButton = document.getElementById("clear");
+
 const operatorSign = ["+", "-", "x", "/"];
+let previousNumber = 0;
+let preOperator = '+';
+let currentNumber = 0;
 
 function buildInitLayout() {
     for (let i = 9; i >= 0; i--) {
@@ -9,6 +18,7 @@ function buildInitLayout() {
         numberButton.setAttribute("class", "btn");
         numberButton.innerHTML = i;
         numbers.appendChild(numberButton);
+        numberButton.addEventListener("click", updateCurrent);
     }
     for (let i = 3; i >= 0; i--) {
         let operatorButton = document.createElement("div");
@@ -16,7 +26,55 @@ function buildInitLayout() {
         operatorButton.setAttribute("class", "btn");
         operatorButton.innerHTML = operatorSign[i];
         operators.appendChild(operatorButton);
+        operators.addEventListener("click", updatePreAnswer);
     }
+    equalButton.addEventListener("click", updatePreAnswer);
+    allClearButton.addEventListener("click", resetCalculator);
+    clearButton.addEventListener("click", removeLastDigit);
+}
+
+function calculate() {
+    if (preOperator == '+') return previousNumber + currentNumber;
+    if (preOperator == '-') return previousNumber - currentNumber;
+    if (preOperator == 'x') return previousNumber * currentNumber;
+    if (preOperator == '/') return previousNumber / currentNumber;   
+}
+
+function updateCurrent(e) {
+    let num = e.target.innerHTML;
+    if (current.innerHTML == "0") current.innerHTML = num;
+    else current.innerHTML += num;
+}
+
+function updatePreAnswer(e) {
+    currentNumber = Number(current.innerHTML);
+    previousNumber = calculate();
+    currentNumber = 0;
+    if (e.target.innerHTML == "=") {
+        currentNumber = previousNumber;
+        previousNumber = 0;
+        preOperator = '+';
+        preAnswer.innerHTML = "0 +";
+        current.innerHTML = currentNumber;
+        return;
+    }
+    preOperator = e.target.innerHTML;
+    preAnswer.innerHTML = previousNumber + ` ` + preOperator;
+    current.innerHTML = "0";
+}
+
+function resetCalculator(e) {
+    currentNumber = 0;
+    previousNumber = 0;
+    preOperator = '+';
+    preAnswer.innerHTML = "No previous number you silly creature!";
+    current.innerHTML = "0";
+}
+
+function removeLastDigit(e) {
+    let temp = current.innerHTML;
+    temp = temp.slice(0, -1);
+    current.innerHTML = (temp == "" ? "0" : temp);
 }
 
 buildInitLayout();
